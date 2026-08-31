709b488b903e46faacc3abc6392203e0d19afadf
vishal421 <47172566+vishal421@users.noreply.github.com>
2026-08-31 19:23:17 +0530

    Update pa_env.py

--- pa_env.py
+++ pa_env.py
@@ -1,3 +1,3 @@
 
 ### this is an example of the API key. Update the X_PAN_KEY variable with your tenant egress IP key ###
-X_PAN_KEY = "0ugr5tfg13__r4T5Lo7r7x2aUmi2xt8lv2oFBaKHxEmH8hgW3pJR"
+X_PAN_KEY = "trerl5rro9__o2j6EOCVgu62yuL8qhO8L7msJ9PVcLLqa9BAng2v"
